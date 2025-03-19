663fb3eacc0a14a04735c8189c8692a7634620a3
boniskizo <boniskizo@gmail.com>
2025-03-19 12:33:16 +0100

    Debug infos livres

--- __init__.py
+++ __init__.py
@@ -80,7 +80,12 @@ def enregistrer_client():
 
 @app.route('/infos_livres')
 def information_livres():
-    return render_template('informations_livres.html') # afficher les infos du livres
+    conn = sqlite3.connect('library.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM Livres;')
+    data = cursor.fetchall()
+    conn.close()
+    return render_template('informations_livres.html', data=data) # afficher les infos du livres
                                                                                                                                    
 if __name__ == "__main__":
   app.run(debug=True)
